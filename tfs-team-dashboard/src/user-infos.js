5c1dbae9582b27ddc4d4bea58d4a34fe650e3502
jsguru-git <js.guru@hotmail.com>
2019-06-16 01:06:24 +1000

    Add user-infos.js file

---
+++ tfs-team-dashboard/src/user-infos.js
@@ -0,0 +1,13 @@
+var PAT = {
+  jsguru: "iegwlaffn5lc4suuhzsbvwsysr7qzgya7pn4ybqppbgdjxrevsmq",
+  collaborator1: "7yauqrgwi3ixm4yhprrnrecabffnvwvf5mtd6dppepglq2vs2v2a",
+  collaborator2: "ofj2bpkvilqyi5zv7a4wxz4bumen3mdivgdwp77gnpyxq4t3zgda"
+};
+
+var gitUser = {
+  jsguru: "jsguru-git",
+  collaborator1: "collaborator1",
+  collaborator2: "collaborator2"
+};
+
+export {PAT, gitUser};
